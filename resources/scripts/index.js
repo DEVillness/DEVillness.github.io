782eb357bfd28b2ddbdf8401d35199033bedcc7e
DEVillness <78412140+DEVillness@users.noreply.github.com>
2022-02-23 14:08:25 +0900

    22022314

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -7,7 +7,3 @@ for (let i = 0; i < headerIcons.length; i++) {
         main.scrollTo({top: sections[i].offsetTop, left: 0, behavior: 'smooth'});
     };
 }
-
-document.getElementById('github').onclick = () => {
-    window.open('https://github.com/DEVillness');
-};
