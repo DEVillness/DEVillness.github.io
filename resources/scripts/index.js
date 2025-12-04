f0bb2a398df23be09551fc9aec7db3b79c04eb6e
Devillness <78412140+Devillness@users.noreply.github.com>
2025-12-04 19:12:48 +0900

    Feat Project Scroll

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -7,3 +7,14 @@ for (let i = 0; i < headerIcons.length; i++) {
         main.scrollTo({top: sections[i].offsetTop, left: 0, behavior: 'smooth'});
     };
 }
+
+const scrolls = document.querySelectorAll("body > main > section.splash.projects > div.container > div.scroll > label > input");
+const projects = document.querySelectorAll("body > main > section.splash.projects > div.container > div.project");
+const projectContainer = document.querySelector("body > main > section.splash.projects > div.container");
+
+for (let i = 0; i < scrolls.length; i++) {
+    scrolls[i].addEventListener("change", () => {
+            projectContainer.scrollTo({top: 0, left: projects[i].offsetLeft, behavior: 'smooth'});
+        }
+    );
+}
